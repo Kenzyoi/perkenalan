7f22723534e3384916743f29c5b0144bcf84d74f
Kenzyoi <vaulgani@gmail.com>
2022-07-27 21:47:23 -0700

    testing

---
+++ Praktikum28Juli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program{
+    static void Main(String[] args){
+        string nama;
+        Console.WriteLine("Masukkan nama : ");
+        nama = Console.ReadLine();
+        Console.WriteLine("Nama saya "+nama);
+        Console.WriteLine("Tahun Lahir : ");
+        int tahunlahir = int.Parse (Console.ReadLine());
+        Console.WriteLine($"Saya Lahir Tahun {tahunlahir}");
+    }
+}
